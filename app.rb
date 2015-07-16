10b204ebee69659f6da677c3c1fe848ba1b33ee9
Nick <nschette@yahoo.com>
2015-07-16 13:34:33 -0500

    added basic files

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
-starting app.rb so i can commit it k thx
+require "pry"
+require "active_record"
+require "sqlite3"
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+
+# So that ActiveRecord explains the SQL it's running in the logs.
+ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
+
+# Models
+require_relative "models/task.rb"
+
+binding.pry
