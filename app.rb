20d71c70574651f8297075d4389102bfbea8e69c
Nick <nschette@yahoo.com>
2015-07-16 13:57:25 -0500

    created database tables

--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@ require "pry"
 require "active_record"
 require "sqlite3"
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'photo_albums.db')
 
 # So that ActiveRecord explains the SQL it's running in the logs.
 ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
 
 # Models
-require_relative "models/task.rb"
+require_relative "models/photographer.rb"
+require_relative "models/photo.rb"
+require_relative "models/album.rb"
 
-binding.pry
+# Database Setup file
+require_relative "database_setup.rb"
---
+++ database_setup.rb
@@ -0,0 +1,30 @@
+unless ActiveRecord::Base.connection.table_exists?(:photographers)
+  ActiveRecord::Base.connection.create_table :photographers do |table|
+    table.text :name
+    table.text :location
+  end
+end
+
+# photos table
+unless ActiveRecord::Base.connection.table_exists?(:photos)
+  ActiveRecord::Base.connection.create_table :photos do |table|
+    table.text :title
+    table.text :location
+    table.integer :photographer_id
+  end
+end
+
+# albums table
+unless ActiveRecord::Base.connection.table_exists?(:albums)
+  ActiveRecord::Base.connection.create_table :albums do |table|
+    table.text :title
+  end
+end
+
+# albums_photos table
+unless ActiveRecord::Base.connection.table_exists?(:albums_photos)
+  ActiveRecord::Base.connection.create_table :albums_photos, id: false do |table|
+    table.integer :photo_id
+    table.integer :album_id
+  end
+end
