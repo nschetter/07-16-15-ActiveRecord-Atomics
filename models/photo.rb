96e7cd0ab9aa2e368536a258132ddfc41a7e61a3
Nick <nschette@yahoo.com>
2015-07-16 14:42:59 -0500

    added top_photo? method

--- models/photo.rb
+++ models/photo.rb
@@ -2,4 +2,15 @@ class Photo < ActiveRecord::Base
   validates :title, presence: true
   validates :photographer_id, presence: true
   has_and_belongs_to_many :albums
+
+  # Determines if a photo exists in many albums
+  #
+  # Returns true/false Boolean
+  def top_photo?
+    if self.albums.length >= 3
+      true
+    else
+      false
+    end
+  end
 end
